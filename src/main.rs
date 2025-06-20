eac6c8b1cf222dd4506cbd63bc25a3528f5636c9
jade-istaken <jade.dergevorkian@gmail.com>
2025-06-19 17:56:34 -0700

    implemented previous pixel array

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use image::ImageReader;
-use std::{fs::write, os::unix::process};
+use std::{fs::write};
 use itertools::concat;
 
 const MAGIC_BYTES: [u8; 4] = [113,111,105,102];
 const END_BYTES: [u8;8] = [0,0,0,0,0,0,0,1];
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct RGBAPixel {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8
+}
+struct RGBPixel {
+    r: u8,
+    g: u8,
+    b: u8
+}
 
 fn main() {
     let img = ImageReader::open("testcard.png").unwrap().decode().unwrap();
@@ -26,16 +39,32 @@ fn construct_header(width: u32, height: u32) -> Vec<u8> {
 // }
 
 fn convert_bytes(img_bytes: Vec<u8>) -> Vec<u8> {
+    let mut pixel_array: [RGBAPixel;64] = [RGBAPixel{r:0,g:0,b:0,a:0};64];
     let length = img_bytes.len();
     let mut processed_bytes: Vec<u8> = vec![];
     let mut index = 0;
     while index < length {
-        processed_bytes.append(&mut qoi_op_rgba(img_bytes[index],img_bytes[index+1],img_bytes[index+2],img_bytes[index+3]));
+        let current_pixel = RGBAPixel{r:img_bytes[index],g:img_bytes[index+1],b:img_bytes[index+2],a:img_bytes[index+3]};
+        let pixel_hash = calculate_hash(current_pixel);
+        if pixel_array[pixel_hash] == current_pixel {
+            processed_bytes.push(qoi_op_index(pixel_hash));
+        } else {
+            processed_bytes.append(&mut qoi_op_rgba(current_pixel));
+            pixel_array[pixel_hash] = current_pixel;
+        }
         index += 4;
     }
     processed_bytes
 }
 
-fn qoi_op_rgba(r:u8, g:u8, b:u8, a:u8) -> Vec<u8> {
-    vec![255,r,g,b,a]
+fn qoi_op_rgba(pixel: RGBAPixel) -> Vec<u8> {
+    vec![255,pixel.r, pixel.g, pixel.b, pixel.a]
+}
+
+fn calculate_hash(pixel: RGBAPixel) -> usize{
+    (pixel.r as usize * 3 + pixel.g as usize * 5 + pixel.b as usize * 7 + pixel.a as usize * 11) % 64
+}
+
+fn qoi_op_index(pixel_hash: usize) -> u8 {
+    (0b00000000 + pixel_hash).try_into().unwrap()
 }
